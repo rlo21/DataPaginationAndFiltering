a1a261fbe8099c8c2cbc7c862952ab9544452ff6
Rosy Lopez <64448346+rlo21@users.noreply.github.com>
2022-06-04 12:48:59 -0500

    Added Start Files

---
+++ js/script.js
@@ -0,0 +1,33 @@
+/*
+Treehouse Techdegree:
+FSJS Project 2 - Data Pagination and Filtering
+Author of script.js: Rosa Lopez
+*/
+
+
+
+/*
+For assistance:
+   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
+   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+*/
+
+
+
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+function showPage () {
+   console.log();
+}
+
+
+/*
+Create the `addPagination` function
+This function will create and insert/append the elements needed for the pagination buttons
+*/
+
+
+
+// Call functions
