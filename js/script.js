dfe8af8d20c70bf03bf3930299b820b18cf6221d
Rosy Lopez <64448346+rlo21@users.noreply.github.com>
2022-06-05 22:05:46 -0500

    ShowPage Function Update

--- js/script.js
+++ js/script.js
@@ -12,16 +12,49 @@ For assistance:
    Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
 */
 
+/*
+Create an event listener to listen for clicks on the link-list variable that you created earlier.
+*/
 
+// Global Variables //
+let itemsPerPage = 9;
 
 /*
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
-function showPage () {
-   console.log();
-}
+function showPage (list,page) {
+   
+   for (let i = 0; i < list.length; i++) {
+      // Variables
+      const indexStart = (page * itemsPerPage) - itemsPerPage;
+      const indexEnd = page * itemsPerPage;
+      const linkList = document.querySelector(link-list);
+      let studentList = document.selectElementByClassName(studnet-list);
+      studentList.document.innerHTML = '';
 
+      //loop over list parameter
+      while ( i >= indexStart &&  i <= indexEnd) {
+         let studentItem = `  
+         <li class="student-item cf">
+            <div class="student-details">
+               <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+               <h3>${list[i].name.first} ${list[i].name.last}</h3>
+               <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+               <span class="date">Joined ${list[i].registered.date}</span>
+            </div>
+         </li>`;
+
+      studentList.insertAdjacentHTML("beforeend", studentItem);
+
+      }
+      
+  
+   }
+};
+function showPage();
 
 /*
 Create the `addPagination` function
